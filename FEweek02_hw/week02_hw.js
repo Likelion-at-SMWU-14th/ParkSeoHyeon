e01fc53d72d8309173ce21cae4d6d7b0b924bd3d
sheven10041 <2411932@sookmyung.ac.kr>
2026-05-03 04:22:21 +0900

    리포스트 기능 구현

--- FEweek02_hw/week02_hw.js
+++ FEweek02_hw/week02_hw.js
@@ -15,3 +15,23 @@ likeBtn.addEventListener('click', function() {
     }
     likeCountSpan.innerText = likeCount;
 });
+
+const reposetBtnSet = document.getElementById('repost-btn-set');
+const repostImg = reposetBtnSet.querySelector('img');
+const repostCountSpan = document.getElementById('repost-count');
+
+let repostCount = 11;
+
+reposetBtnSet.addEventListener('click', function() {
+    const isRepostActive = reposetBtnSet.classList.toggle('repost-active');
+
+    if(isRepostActive) {
+        repostCount++;
+        repostImg.src= 'images/check.png';
+        } else {
+            repostCount--;
+            repostImg.src = 'images/repost.png';
+        }
+
+        repostCountSpan.innerText = repostCount;
+}) ;
