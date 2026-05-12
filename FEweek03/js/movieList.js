aed79fd176a81e22735bc27a60d1a8be8d4432d6
sheven10041 <2411932@sookmyung.ac.kr>
2026-05-12 20:27:43 +0900

    fetch API 실습 완료

--- FEweek03/js/movieList.js
+++ FEweek03/js/movieList.js
@@ -14,3 +14,35 @@ const dmovie = movies.filter((movie) => {
 });
 
 console.log("드가 들어가는 장르의 영화 목록:",dmovie);
+
+async function fetchMovies() {
+    try {
+        const response = await fetch("movies.json");
+        const movies = await response.json();
+        const container = document.getElementById("movieContainer");
+        const movieList = movies.filter((movie) => movie.genre === "드라마");
+
+        movieList.forEach((movie) => {
+            const card = document.createElement("div");
+            card.className ="card";
+
+            card.innerHTML = `
+                  <img src = "${movie.poster}" alt="${movie.title}">
+                  <div class="card-body">
+                      <h2 class="card-title">${movie.title}</h2>
+                      <p class = "card-rating">${movie.rating}</p>
+                      <p class = "card-description">${movie.description}</p>
+                    </div>
+        `;
+        container.appendChild(card);
+
+
+
+        });
+
+    } catch(error) {
+        console.log("에러 발생:",error);
+    }
+    
+}
+fetchMovies();
