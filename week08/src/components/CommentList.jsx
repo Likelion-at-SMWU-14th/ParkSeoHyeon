2369c275c66629d73489d32101b545d7b82a916f
sheven10041 <2411932@sookmyung.ac.kr>
2026-07-14 15:52:39 +0900

    feat:CommentPage 페이지 구현

---
+++ week08/src/components/CommentList.jsx
@@ -0,0 +1,24 @@
+import { COMMENT_DATA } from "../constant/comment";
+import Comment from "./Comment";
+import styled from "styled-components";
+
+const CommentList = () => {
+  const comments = COMMENT_DATA;
+  return (
+    <CommentWrapper>
+      {comments.map((comment) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </CommentWrapper>
+  );
+};
+
+export default CommentList;
+
+const CommentWrapper = styled.div`
+  display: grid;
+  width: 100%;
+  padding: 2rem 3.5rem;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+  gap: 1.25rem 1.75rem;
+`;
