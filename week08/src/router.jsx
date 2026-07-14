5cf65c76a49eea299e97fe536d1afd6bd29369dc
sheven10041 <2411932@sookmyung.ac.kr>
2026-07-14 15:46:12 +0900

    feat:라우터 및 레이아웃 설정 완료

--- week08/src/router.jsx
+++ week08/src/router.jsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import CommentPage from "./pages/CommentPage";
+import DetailPage from "./pages/DetailPage";
+import WritePage from "./pages/WritePage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <CommentPage />,
+      },
+      {
+        path: "datail/:id",
+        element: <DetailPage />,
+      },
+      {
+        path: "write",
+        element: <WritePage />,
+      },
+    ],
   },
 ]);
 
