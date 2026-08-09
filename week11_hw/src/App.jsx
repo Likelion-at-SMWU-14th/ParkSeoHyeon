bd2bc42d1c9945c134ff4d6dfdef330775117595
sheven10041 <2411932@sookmyung.ac.kr>
2026-08-09 12:21:53 +0900

    feat:ContextAPI로 prop drilling 해결

---
+++ week11_hw/src/App.jsx
@@ -0,0 +1,33 @@
+import { useContext } from "react";
+import Content from "./components/Content";
+import { styled } from "styled-components";
+import PartContext from "./contexts/PartContext";
+
+function App() {
+  const { part } = useContext(PartContext);
+
+  return (
+    <Wrapper>
+      <div className="title">
+        숙명여대 멋쟁이사자처럼 <span>{part}</span> 파트 구성원
+      </div>
+      <Content />
+    </Wrapper>
+  );
+}
+
+export default App;
+
+const Wrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  .title {
+    text-align: center;
+    padding: 50px 0;
+    font-size: 30px;
+    font-weight: 700;
+    span {
+      color: #ee7521;
+    }
+  }
+`;
