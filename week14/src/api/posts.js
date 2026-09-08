bd13c1198c6710432560d28c12bf143eabea3fd5
sheven10041 <2411932@sookmyung.ac.kr>
2026-09-08 20:08:05 +0900

    feat:posts CRUD API 함수 작성

---
+++ week14/src/api/posts.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
+export const createPost = async (newPost) => {
+  const { data } = await axios.post(`${BASE_URL}/posts`, newPost);
+  return data;
+};
+
+export const fetchPosts = async () => {
+  const { data } = await axios.post(`${BASE_URL}/posts`);
+  return data;
+};
+
+export const updatePost = async ({ id, ...updateFields }) => {
+  const { data } = await axios.post(`${BASE_URL}/posts/${id}`, updateFields);
+  return data;
+};
+
+export const deletePost = async (id) => {
+  await axios.delete(`${BASE_URL}/posts/${id}`);
+  return id;
+};
