ea0585989160eb1c408be6dc88dd823c8cfc2fb3
sheven10041 <2411932@sookmyung.ac.kr>
2026-09-13 15:06:02 +0900

    feat: 사용자 기능 컴포넌트 연결

--- week14_hw/src/App.jsx
+++ week14_hw/src/App.jsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import MyPage from "./components/MyPage";
+import SignUpForm from "./components/SignUpForm";
+import UpdateUserForm from "./components/UpdateUserForm";
+import DeleteUserButton from "./components/DeleteUserButton";
 
 function App() {
-  return <div></div>;
+  const userId = 1;
+
+  return (
+    <main>
+      <h1>TanStack Query 과제</h1>
+
+      <SignUpForm />
+
+      <hr />
+
+      <MyPage userId={userId} />
+      <UpdateUserForm userId={userId} />
+      <DeleteUserButton userId={userId} />
+    </main>
+  );
 }
 
 export default App;
