99ed0864d5af7f5bedcfd141ee517ef975e2adce
sheven10041 <2411932@sookmyung.ac.kr>
2026-09-15 10:58:02 +0900

    fix: user API axios 요청으로 수정

--- week14_hw/src/api/users.js
+++ week14_hw/src/api/users.js
@@ -1,36 +1,37 @@
+import axios from "axios";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
+
 // 회원가입
 export const createUser = async ({ username, password }) => {
-  console.log("POST /api/users", { username, password });
-
-  return {
-    id: 1,
+  const { data } = await axios.post(`${BASE_URL}/users`, {
     username,
-  };
+    password,
+  });
+
+  return data;
 };
 
 // 마이페이지 조회
 export const fetchUser = async (userId) => {
-  console.log(`GET /api/users/${userId}`);
+  const { data } = await axios.get(`${BASE_URL}/users/${userId}`);
 
-  return {
-    id: userId,
-    username: "seohyun",
-  };
+  return data;
 };
 
 // 개인정보 수정
 export const updateUser = async ({ userId, ...updatedFields }) => {
-  console.log(`PUT /api/users/${userId}`, updatedFields);
+  const { data } = await axios.put(
+    `${BASE_URL}/users/${userId}`,
+    updatedFields,
+  );
 
-  return {
-    id: userId,
-    ...updatedFields,
-  };
+  return data;
 };
 
 // 회원 탈퇴
 export const deleteUser = async (userId) => {
-  console.log(`DELETE /api/users/${userId}`);
+  await axios.delete(`${BASE_URL}/users/${userId}`);
 
   return userId;
 };
