0f8c47ccc4df39a8e74662ba018c2912e38e8401
sheven10041 <2411932@sookmyung.ac.kr>
2026-09-24 17:21:16 +0900

    feat:Todo 데이터 타입 정의

---
+++ week16/week16/src/types/todo.ts
@@ -0,0 +1,10 @@
+export type TodoPriority = "low" | "medium" | "high";
+
+export type TodoFilter = "all" | "active" | "completed";
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  priority: TodoPriority;
+}
